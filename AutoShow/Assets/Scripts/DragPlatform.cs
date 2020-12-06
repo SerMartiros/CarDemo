0c0b7f77a5d9cff5ffdb61f4b42eb4b34c70d7f3
SerMartiros <sermartiros@outlook.com>
2020-12-06 19:57:44 +0100

    Add Variable

--- AutoShow/Assets/Scripts/DragPlatform.cs
+++ AutoShow/Assets/Scripts/DragPlatform.cs
@@ -6,11 +6,13 @@ public class DragPlatform : MonoBehaviour
 {
     [SerializeField]
     private Toggle checker;
+    [SerializeField]
+    private float dragAmount;
     public void OnMouseDrag()
     {
         if (checker.isOn)
         {
-            float mouseX = Input.GetAxis("Mouse X") * 5f;
+            float mouseX = Input.GetAxis("Mouse X") * dragAmount;
             this.gameObject.transform.Rotate(Vector3.down, mouseX);
         }
     }
